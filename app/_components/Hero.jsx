f50eff5a73282adbf0b5433e9b4836bfcf8691db
melika760 <melikaaatafazoli@gmail.com>
2024-10-08 16:00:11 +0100

    adding more responsiveness

--- app/_components/Hero.jsx
+++ app/_components/Hero.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Image from 'next/image'
 import { Button } from '@/components/ui/button'
 import Link from 'next/link'
+
 const Hero = () => {
   return (
     <section>
@@ -9,7 +10,8 @@ const Hero = () => {
     <div className="grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-16">
       <div className="relative overflow-hidden rounded-lg sm:h-80 lg:order-last md:h-full ">
         <div className=' w-full  h-full  z-0'>
-        <Image src="/hero.png" className='object-contain w-full -rotate-45 ' width={550} height={250}/>
+        <Image src="/transport.JPG" className='object-contain w-full ' width={550} height={250}/>
+        {/* <Image src="/hero.png" className='object-contain w-full -rotate-45 ' width={550} height={250}/> */}
         {/* <Image src="/hero-bg.png" className=' max-sm:hidden absolute -top-24  -right-1/3 -z-10 w-full h-screen  overflow-hidden' width={350} height={390}/> */}
         </div>
      
--- app/_components/MessageInput.jsx
+++ app/_components/MessageInput.jsx
@@ -3,27 +3,6 @@ import { Image, Paperclip, ScrollText, Send, Smile } from 'lucide-react'
 import { storage,db, auth } from '@/config/firebaseConfig'
 import{ ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage'
 import React, { useEffect, useState } from 'react'
-import {
-  Sheet,
-  SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from "@/components/ui/sheet"
-
-
-import { format } from "date-fns"
-import { Calendar as CalendarIcon } from "lucide-react"
- 
-import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
-import { Calendar } from "@/components/ui/calendar"
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover"
 import { Label } from '@/components/ui/label'
 import EmojiPicker from 'emoji-picker-react';
 import { collection, addDoc, getDoc, getDocs, query, where } from 'firebase/firestore'
@@ -33,7 +12,7 @@ import { toast } from 'sonner'
 import { useRouter } from 'next/navigation'
 
 const MessageInput = ({ sendMessage, message, setMessage,image,setImage,selectedChatroom}) => {
-  const[date,setDate]=useState(Date())
+  const[date,setDate]=useState("")
   const [file, setFile] = useState(null);
   const [file2, setFile2] = useState(null);
   const [uploadProgress, setUploadProgress] = useState(null);
@@ -193,39 +172,34 @@ const MessageInput = ({ sendMessage, message, setMessage,image,setImage,selected
     <div className='flex items-center p-4 border-t border-gray-200 relative'>
    
  {image ? <Image className='text-green-600 cursor-pointer mr-2 text-xs'/>: <Paperclip className='text-gray-500 cursor-pointer mr-2 text-xs' onClick={() => document.getElementById('my_modal_3').showModal()}/>}
-  {!disable &&  <Sheet>
-  <SheetTrigger><h3 className='text-primary'><ScrollText className='text-gray-500 cursor-pointer mr-2 text-xs'/></h3></SheetTrigger>
-  <SheetContent className="snap-y   md:h-[800px] h-[400px]" side="top">
-    <SheetHeader>
-      <SheetTitle className="text-center items-center">Ready for making contract?</SheetTitle>
-      <SheetDescription className="m-12 p-12">
-      <Label htmlFor="TotalAmount">Enter Final Agreed Amount:<Input placeholder="$..." type="text" className="m-3 mt-5 mb-5"onChange={AmountChange} onBlur={AmountBlur} value={enteredAmount}/></Label>
-      <Label htmlFor="DeliveryDate">Select Agreed Delivery Date:
-      </Label>
-      <Popover>
-        <PopoverTrigger asChild>
-          <Button
-            variant={"outline"}
-            className={cn(
-              "w-full justify-start text-left font-normal m-3 mt-5 mb-5",
-            !date && "text-muted-foreground"
-            )}
-          >
-            <CalendarIcon className="mr-2 h-4 w-4" />
-            {date ? format(date, "PPP") : <span>Pick a date</span>}
-          </Button>
-        </PopoverTrigger>
-        <PopoverContent className="w-auto p-0">
-          <Calendar
-              mode="single"
-              selected={date}
-              onSelect={(newDate) => {
-                setDate(newDate);
-              }}
-              initialFocus
+
+{!disable && <h3 className='text-primary'><ScrollText className='text-gray-500 cursor-pointer mr-2 text-xs'onClick={()=>document.getElementById("my_modal_4").showModal()}/></h3>}
+{disable && <h3 className='text-primary'><ScrollText className='text-green-500 cursor-pointer mr-2 text-xs'onClick={()=>toast("You've already made this contract!")}/></h3>}
+  
+
+<button onClick={() => setShowEmojiPicker(!showEmojiPicker)}>
+        <Smile className='text-gray-500 cursor-pointer mr-2 text-xs'/>
+      </button>
+  <Input type="text" className="flex-1 border-none p-2 outline-none" placeholder="Type a message..." value={message}
+        onChange={(e) => setMessage(e.target.value)}/>
+  <Send className='text-gray-500 cursor-pointer ml-2' onClick={() => sendMessage()}/>
+  {showEmojiPicker && (
+        <div className='absolute right-0 bottom-full p-2'>
+          <EmojiPicker
+            onEmojiClick={handleEmojiClick}
+            disableAutoFocus={true}
           />
-        </PopoverContent>
-      </Popover>
+        </div>
+      )}
+      <dialog id='my_modal_4' className='modal'>
+      <div className="modal-box relative p-6 bg-white rounded-lg shadow-lg max-w-lg w-full">
+        <h2>Ready for making contract?</h2>
+        <form method="dialog" className="space-y-4">
+        <Label htmlFor="TotalAmount">Enter Final Agreed Amount:<Input placeholder="$..." type="text" className="m-3 mt-5 mb-5"onChange={AmountChange} onBlur={AmountBlur} value={enteredAmount}/></Label>
+      <Label htmlFor="DeliveryDate">Select Agreed Delivery Date:
+        <Input placeholder="Enter your delivery date MM/DD/YY" value={date} onChange={e=>setDate(e.target.value)}/>
+      </Label>
+  
       <div className='space-y-2'>   
        <Label>
       Upload your Official Contract:
@@ -244,35 +218,22 @@ const MessageInput = ({ sendMessage, message, setMessage,image,setImage,selected
  
            <div 
         onClick={()=>handleuploads()} 
-        className="btn btn-sm btn-primary w-[200px]  cursor-pointer float-end text-center py-2 mt-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition-colors duration-300"
+        className="btn btn-sm btn-primary w-[200px]  cursor-pointer text-center py-2 mt-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition-colors duration-300"
       >
         Submit contract
       </div>
   
       </div>
    
-      </SheetDescription>
-      
-    </SheetHeader>
-  </SheetContent>
-</Sheet>} 
-{disable && <h3 className='text-primary'><ScrollText className='text-green-500 cursor-pointer mr-2 text-xs'onClick={()=>toast("You've already made this contract!")}/></h3>}
-  
-
-<button onClick={() => setShowEmojiPicker(!showEmojiPicker)}>
-        <Smile className='text-gray-500 cursor-pointer mr-2 text-xs'/>
-      </button>
-  <Input type="text" className="flex-1 border-none p-2 outline-none" placeholder="Type a message..." value={message}
-        onChange={(e) => setMessage(e.target.value)}/>
-  <Send className='text-gray-500 cursor-pointer ml-2' onClick={() => sendMessage()}/>
-  {showEmojiPicker && (
-        <div className='absolute right-0 bottom-full p-2'>
-          <EmojiPicker
-            onEmojiClick={handleEmojiClick}
-            disableAutoFocus={true}
-          />
-        </div>
-      )}
+        </form>
+        <button
+      onClick={() => document.getElementById('my_modal_4').close()}
+      className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2 text-gray-500 hover:text-gray-700 transition-transform transform hover:scale-110"
+    >
+      ✕
+    </button>
+      </div>
+      </dialog>
    
       <dialog id="my_modal_3" className="modal ">
   <div className="modal-box relative p-6 bg-white rounded-lg shadow-lg max-w-lg w-full">
