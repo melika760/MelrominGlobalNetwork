d66cb52a61efc98d7ea4eed967b5b609e9dd0f67
melika760 <melikaaatafazoli@gmail.com>
2024-10-12 16:57:41 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -4,8 +4,8 @@ import Hero from "./_components/Hero";
 import Aboutus from "./_components/Aboutus";
 import Getstarted from "./_components/Getstarted";
 import Footer from "./_components/Footer";
+import Works from "./_components/Works";
 
-import Works from "./_components/works";
 export default function Home() {
   return (
    <div>
