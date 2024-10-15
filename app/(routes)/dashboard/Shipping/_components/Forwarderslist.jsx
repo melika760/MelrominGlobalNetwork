2196e850368afbe0cdbb8fd83ad9174d82bcfdcb
melika760 <melikaaatafazoli@gmail.com>
2024-10-15 12:40:18 +0100

    make forwarderlist responsive

--- app/(routes)/dashboard/Shipping/_components/Forwarderslist.jsx
+++ app/(routes)/dashboard/Shipping/_components/Forwarderslist.jsx
@@ -48,7 +48,7 @@ const Forwarderslist = ({ selectedCountry, inquiery, transportation }) => {
         <div className="md:p-12 p-4">
             <h2 className="text-primary font-bold text-xl mb-8">Forwarders List</h2>
             <div className='p-6'>
-                <div className='grid grid-cols-4 gap-4 p-4 border-b-2 bg-gray-100'>
+                <div className='md:grid-cols-3 md:grid hidden items-center justify-between p-4 border-b bg-gray-200'>
                     <h3 className='font-bold'>Forwarder</h3>
                     <h3 className='font-bold'>Country</h3>
                     <h3 className='font-bold'>More</h3>
@@ -57,22 +57,35 @@ const Forwarderslist = ({ selectedCountry, inquiery, transportation }) => {
                     {forwarders.length > 0 ? (
                         forwarders.map((forwarder, index) => (
                             <div key={index} className='flex flex-col justify-between'>
-                                <div className='grid grid-cols-4 gap-4 p-4 border-b-2 items-center relative'>
-                                    <p>{forwarder.name}</p>
-                                    <p>{forwarder.country}</p>
-                                    <Button onClick={() => toggleMoreInfo(index)} className="w-[55px]" variant={"ghost"}>
+                                <div className='md:grid-cols-4 grid grid-cols-1 items-center md:justify-between gap-4 p-4 border-b hover:bg-gray-50 transition-colors duration-300 relative'>
+                                    <p className='text-sm sm:text-base text-gray-800'><span className='font-semibold text-sm sm:text-base text-gray-700 md:hidden sm:block'>Forwarders' name:</span>{forwarder.name}</p>
+                                    <p className='text-sm sm:text-base text-gray-800'><span className='font-semibold text-sm sm:text-base text-gray-700 md:hidden sm:block'>Country:</span>{forwarder.country}</p>
+                                    <span className='font-semibold text-sm sm:text-base text-gray-700 md:hidden sm:flex'>More info
+                 </span>
+                                   <span><Button onClick={() => toggleMoreInfo(index)} className="w-[55px]" variant={"ghost"}>
                                         {moreInfo[index] ? <ArrowBigUp className='text-primary cursor-pointer'/> : <ArrowBigDown className='text-primary cursor-pointer'/>}
-                                    </Button>
-                                    <Button onClick={() => sendData(forwarder)} className="absolute right-9">Choose</Button>
-                                </div>
-                                <div>
+                                    </Button></span> 
+                                    <div className='md:hidden block'>
                                     {moreInfo[index] &&
-                                        <div className='p-4'>
-                                            <p className='mb-5'><strong>About Company:</strong> {forwarder.description}</p>
-                                            <p><strong>Experience:</strong> {forwarder.Experience} Years</p>
+                                        <div className=''>
+                                            <p className='text-sm sm:text-base text-gray-800 mb-5'><strong>About Company:</strong> {forwarder.description}</p>
+                                            <p className='text-sm sm:text-base text-gray-800'><strong>Experience:</strong> {forwarder.Experience} Years</p>
                                         </div>
                                     }
+                                      
                                 </div>
+                                    <Button onClick={() => sendData(forwarder)} className="md:w-[250px] ">Choose</Button>
+                                </div>
+                                <div className='max-sm:hidden block'>
+                                    {moreInfo[index] &&
+                                        <div className='p-2'>
+                                            <p className='text-sm sm:text-base text-gray-800 mb-5'><strong>About Company:</strong> {forwarder.description}</p>
+                                            <p className='text-sm sm:text-base text-gray-800'><strong>Experience:</strong> {forwarder.Experience} Years</p>
+                                        </div>
+                                    }
+                                      
+                                </div>
+                              
                             </div>
                         ))
                     ) : (<div className="flex flex-col justify-center text-center items-center">
