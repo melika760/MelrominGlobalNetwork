d1f9e153ea63ab8848a1dd369ff835807cb4021c
melika760 <melikaaatafazoli@gmail.com>
2024-11-21 15:29:46 +0000

    add more animations

--- app/_components/Getstarted.jsx
+++ app/_components/Getstarted.jsx
@@ -1,16 +1,19 @@
+"use client"
 import { LogIn,MessageSquareText,ReceiptText,ScrollText} from 'lucide-react'
 import React from 'react'
+import useScroll from './_hooks/use-scroll'
 
 const Getstarted = () => {
+  useScroll()
   return (
     <section className='my-20'id='start'>
   <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
     <div className="mx-auto max-w-lg text-center">
-      <h2 className="text-3xl font-extrabold sm:text-4xl  bg-gradient-to-r from-blue-900  to-blue-500 bg-clip-text text-transparent ">Get Started</h2>
+      <h2 className="text-3xl font-extrabold sm:text-4xl  bg-gradient-to-r from-blue-900  to-blue-500 bg-clip-text text-transparent " data-animation="animate-fade-in">Get Started</h2>
     </div>
 
     <div className="mt-8 grid lg:grid-cols-2 grid-cols-1 lg:gap-20 gap-10">
-    <div className='border shadow-lg bg-gray-100 md:py-20 py-10 md:px-20 px-5 rounded-md'>
+    <div className='border shadow-lg bg-gray-100 md:py-20 py-10 md:px-20 px-5 rounded-md'data-animation="animate-turnleft">
 <h2 className='font-bold text-lg'>Suppliers:</h2>
 <ul className='my-7'>
     <li className='flex py-2'><LogIn className='mr-2 h-5 w-4 '/> Sign up on our site</li>
@@ -20,7 +23,7 @@ const Getstarted = () => {
 </ul>
 <p className='font-bold text-lg text-black'>Bingo! We guarantee your shipment will arrive on time.</p>
     </div>
-    <div className='border shadow-lg bg-gray-100 md:py-20 py-10 md:px-20 px-5 rounded-md'>
+    <div className='border shadow-lg bg-gray-100 md:py-20 py-10 md:px-20 px-5 rounded-md'data-animation="animate-turnright">
 <h2 className='font-bold text-lg'>Forwarders:</h2>
 <ul className='my-7'>
     <li className='flex py-2'><LogIn className='mr-2 h-5 w-4'/> Sign up on our site</li>
