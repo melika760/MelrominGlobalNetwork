2bffb02ae1ebd63c578838b13585ae6fc987f4b2
melika760 <melikaaatafazoli@gmail.com>
2025-01-14 12:28:56 +0000

    add some css

--- app/_components/Hero.jsx
+++ app/_components/Hero.jsx
@@ -14,7 +14,7 @@ const Hero = () => {
     className="relative mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8"
   >
     <div className="max-w-xl  ltr:sm:text-left rtl:sm:text-right ">
-      <h1 className=" text-3xl font-extrabold sm:text-5xl  bg-gradient-to-r from-blue-900 via-blue-500 to-purple-500 bg-clip-text text-transparent ">
+      <h1 className=" text-3xl font-extrabold sm:text-5xl  text-black ">
       The World’s First Partnership in Logistics and Supply Chain
       
       </h1>
@@ -26,14 +26,14 @@ const Hero = () => {
       <div className="mt-8 flex  gap-4 text-center">
         <a
           href="#start"
-          className="block w-full rounded bg-blue-500 px-12 py-3 text-sm font-medium text-white shadow hover:bg-blue-500 focus:outline-none focus:ring active:bg-primary sm:w-auto max-sm:px-3"
+          className="block w-full rounded bg-[#6930c3] px-12 py-3 text-sm font-medium text-white shadow hover:bg-[#6930c3] focus:outline-none focus:ring active:bg-primary sm:w-auto max-sm:px-3"
         >
           Get Started
         </a>
 
         <a
           href="#work"
-          className="block w-full rounded bg-white px-12 py-3 text-sm font-medium text-primary shadow hover:text-primary focus:outline-none focus:ring active:text-primary sm:w-auto max-sm:px-3"
+          className="block w-full rounded bg-white px-12 py-3 text-sm font-medium text-[#6930c3] shadow hover:text-[#6930c3] focus:outline-none focus:ring active:text-primary sm:w-auto max-sm:px-3"
         >
           Learn More
         </a>
