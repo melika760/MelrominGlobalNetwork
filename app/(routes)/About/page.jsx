79949d0f5265d94c9d2a8ebd1ddf9b5f0b87ed30
melika760 <melikaaatafazoli@gmail.com>
2025-05-18 15:58:07 +0100

    adding meta tags

--- app/(routes)/About/page.jsx
+++ app/(routes)/About/page.jsx
@@ -2,7 +2,13 @@ import AboutPage from '@/app/_components/AboutPage'
 import Footer from '@/app/_components/Footer'
 import Header from '@/app/_components/Header'
 import React from 'react'
-
+export const metadata = {
+  title: "About Us | Melromin Global Network",
+  description: "Learn more about Melromin Global Network, our mission to innovate freight forwarding, and how we connect businesses with top-tier logistics providers worldwide.",
+  alternates: {
+    canonical: "https://melromin.co.uk/about",
+  },
+};
 const About = () => {
   return (
     <>
--- app/page.js
+++ app/page.js
@@ -6,7 +6,13 @@ import Getstarted from "./_components/Getstarted";
 import Footer from "./_components/Footer";
 import Works from "./_components/Works";
 import NewWork from "./_components/NewWork";
-
+export const metadata = {
+  title: " Melromin Global Network",
+  description: "Learn more about Melromin Global Network, our mission to innovate freight forwarding, and how we connect businesses with top-tier logistics providers worldwide.",
+  alternates: {
+    canonical: "https://melromin.co.uk/about",
+  },
+};
 export default function Home() {
   return (
    <div>
